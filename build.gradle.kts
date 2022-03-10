ca43099d21eef5221d7155ff5e032ae250915d8f
Yuri Schimke <yuri@schimke.ee>
2022-03-10 08:53:38 +0000

    Cleanup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,4 @@
-import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
-
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-
 plugins {
-    alias(libs.plugins.benmanesversions)
-    alias(libs.plugins.versioncatalogupdate)
     alias(libs.plugins.androidapplication) apply false
     alias(libs.plugins.kotlinandroid) apply false
     alias(libs.plugins.kotlinkapt) apply false
@@ -16,23 +10,4 @@ tasks.create<Delete>("clean") {
     delete = setOf(
         rootProject.buildDir
     )
-}
-
-tasks.withType<DependencyUpdatesTask> {
-    val alpha = ".*-alpha.*".toRegex()
-    val beta = ".*-beta.*".toRegex()
-    rejectVersionIf {
-        (candidate.version.matches(alpha) && !(currentVersion.matches(alpha)) ||
-                (candidate.version.matches(beta) &&
-                        !(currentVersion.matches(alpha) || currentVersion.matches(beta)
-                                )
-                        )
-                )
-    }
-}
-
-versionCatalogUpdate {
-    keep {
-        keepUnusedVersions.set(true)
-    }
 }
