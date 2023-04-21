93dc33f94ffa7d72d4f55559f5010589d41b0d6e
Yuri Schimke <yuri@schimke.ee>
2023-04-21 13:33:52 +0100

    Fixes

--- wear/src/main/kotlin/com/google/wear/soyted/RememberWearActivity.kt
+++ wear/src/main/kotlin/com/google/wear/soyted/RememberWearActivity.kt
@@ -16,11 +16,19 @@
 
 package com.google.wear.soyted
 
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavHostController
+import androidx.wear.compose.material.Text
 import androidx.wear.compose.navigation.rememberSwipeDismissableNavController
 import com.google.wear.soyted.ui.home.RememberWearAppScreens
 import com.google.wear.soyted.ui.util.JankPrinter
@@ -39,15 +47,29 @@ class RememberWearActivity : ComponentActivity() {
         setContent {
             navController = rememberSwipeDismissableNavController()
 
-            RememberWearAppScreens(navController = navController)
+            val context = LocalContext.current
+            val isWatch =
+                context.packageManager.hasSystemFeature(PackageManager.FEATURE_WATCH)
+            if (isWatch) {
+                RememberWearAppScreens(navController = navController)
 
-            LaunchedEffect(Unit) {
-                navController.currentBackStackEntryFlow.collect {
-                    jankPrinter.setRouteState(route = it.destination.route)
+                LaunchedEffect(Unit) {
+                    navController.currentBackStackEntryFlow.collect {
+                        jankPrinter.setRouteState(route = it.destination.route)
+                    }
                 }
+            } else {
+                NotAWatch()
             }
         }
 
         jankPrinter.installJankStats(activity = this)
+    }
+
+}
+
+@Composable fun NotAWatch() {
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Text("Soyted is a Wear OS App")
     }
 }
